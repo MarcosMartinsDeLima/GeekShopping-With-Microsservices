78af275f9e45bbde29096c004ac4193039921dd3
MarcosMartinsDeLima <0timetraveler@gmail.com>
2024-01-16 17:34:55 -0300

    configurando

--- Services/GeekShopping.IdentityServer/Program.cs
+++ Services/GeekShopping.IdentityServer/Program.cs
@@ -1,8 +1,37 @@
+using GeekShopping.IdentityServer.Configuration;
+using GeekShopping.IdentityServer.Model;
+using GeekShopping.IdentityServer.Model.Context;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// connection to database
+var connection = builder.Configuration["MysqlConnection:MysqlConnectionString"];
+builder.Services.AddDbContext<MysqlContext>(options => options.UseMySql(connection, new MySqlServerVersion(new Version(8,0,5))) );
+
+//identity
+builder.Services.AddIdentity<ApplicationUser,IdentityRole>()
+    .AddEntityFrameworkStores<MysqlContext>()
+    .AddDefaultTokenProviders();
+var builderServices = builder.Services.AddIdentityServer(options => {
+    options.Events.RaiseErrorEvents = true;
+    options.Events.RaiseInformationEvents = true;
+    options.Events.RaiseSuccessEvents = true;
+    options.Events.RaiseFailureEvents = true;
+    options.EmitStaticAudienceClaim = true;
+})
+    .AddInMemoryIdentityResources(IdentityConfiguration.IdentityResources)
+    .AddInMemoryApiScopes(IdentityConfiguration.ApiScopes)
+    .AddInMemoryClients(IdentityConfiguration.Clients)
+    .AddAspNetIdentity<ApplicationUser>();
+
+
+
+builderServices.AddDeveloperSigningCredential();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -17,6 +46,8 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseIdentityServer();
 
 app.UseAuthorization();
 
